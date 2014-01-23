b942cf666528c76062a7141b69a7986bff2f6b5e
knakano <knakano@usc.edu>
2014-01-22 20:43:30 -0800

    assignment 2

--- assignments/assignment-2/results.php
+++ assignments/assignment-2/results.php
@@ -1,81 +1,83 @@
-<head>
-    <link rel="stylesheet" href="css/foundation.css" />
-</head>
-<?php
-/**
- * Created by PhpStorm.
- * User: kalynnakano
- * Date: 1/21/14
- * Time: 5:32 PM
- */
+<html>
+    <head>
+        <link rel="stylesheet" href="css/foundation.css" />
+    </head>
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: kalynnakano
+     * Date: 1/21/14
+     * Time: 5:32 PM
+     */
 
-$host = 'itp460.usc.edu';
-$dbname = 'dvd';
-$user = 'student';
-$pass = 'ttrojan';
+    $host = 'itp460.usc.edu';
+    $dbname = 'dvd';
+    $user = 'student';
+    $pass = 'ttrojan';
 
-$title = $_GET['title']; // $_REQUEST['artist']
+    $title = $_GET['title']; // $_REQUEST['artist']
 
-$pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 
-$sql = "
-SELECT title, rating, genre, format
-FROM dvd_titles
-INNER JOIN ratings
-ON dvd_titles.rating_id = ratings.id
-INNER JOIN genres
-ON dvd_titles.genre_id = genres.id
-INNER JOIN formats
-ON dvd_titles.format_id = formats.id
-WHERE title LIKE ?
-ORDER BY title
-";
+    $sql = "
+    SELECT title, rating, genre, format
+    FROM dvd_titles
+    INNER JOIN ratings
+    ON dvd_titles.rating_id = ratings.id
+    INNER JOIN genres
+    ON dvd_titles.genre_id = genres.id
+    INNER JOIN formats
+    ON dvd_titles.format_id = formats.id
+    WHERE title LIKE ?
+    ORDER BY title
+    ";
 
-$statement = $pdo->prepare($sql);
-$like = '%'.$title.'%';
-$statement->bindParam(1, $like);
+    $statement = $pdo->prepare($sql);
+    $like = '%'.$title.'%';
+    $statement->bindParam(1, $like);
 
-$statement->execute();
-$dvds = $statement->fetchAll(PDO::FETCH_OBJ);
+    $statement->execute();
+    $dvds = $statement->fetchAll(PDO::FETCH_OBJ);
 
-//var_dump($dvds);
-?>
-<body>
-    <div class="row">
-        <div class="large-6 columns" style="padding-top:50px">
-            <form method="get" action="results.php">
-                <div class="row collapse">
-                    <div class="small-10 columns">
-                        <input type="text" placeholder="Search by DVD title" name="title">
+    //var_dump($dvds);
+    ?>
+    <body>
+        <div class="row">
+            <div class="large-6 columns" style="padding-top:50px">
+                <form method="get" action="results.php">
+                    <div class="row collapse">
+                        <div class="small-10 columns">
+                            <input type="text" placeholder="Search by DVD title" name="title">
+                        </div>
+                        <div class="small-2 columns">
+                            <input type="submit" class="button postfix" value="Search">
+                        </div>
                     </div>
-                    <div class="small-2 columns">
-                        <input type="submit" class="button postfix" value="Search">
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-    <div class="row" style="margin-bottom: 100px">
-        <div class="large-12 columns">
-            <h1 style="padding-bottom: 50px">Search results for: '<?php echo $title ?>'</h1>
-            <table>
-                <thead>
-                <tr>
-                    <th>DVD Title</th>
-                    <th>Rating</th>
-                    <th>Genre</th>
-                    <th>Format</th>
-                </tr>
-                </thead>
-                <?php foreach($dvds as $dvd) : ?>
+        <div class="row" style="margin-bottom: 100px">
+            <div class="large-12 columns">
+                <h1 style="padding-bottom: 50px">Search results for: '<?php echo $title ?>'</h1>
+                <table>
+                    <thead>
                     <tr>
-                        <td><?php echo $dvd->title ?></td>
-                        <td><?php echo $dvd->rating ?></td>
-                        <td><?php echo $dvd->genre ?></td>
-                        <td><?php echo $dvd->format ?></td>
+                        <th>DVD Title</th>
+                        <th>Rating</th>
+                        <th>Genre</th>
+                        <th>Format</th>
                     </tr>
-                <?php endforeach ?>
-            </table>
+                    </thead>
+                    <?php foreach($dvds as $dvd) : ?>
+                        <tr>
+                            <td><?php echo $dvd->title ?></td>
+                            <td><?php echo $dvd->rating ?></td>
+                            <td><?php echo $dvd->genre ?></td>
+                            <td><?php echo $dvd->format ?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </table>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
+</html>
--- assignments/assignment-2/search.php
+++ assignments/assignment-2/search.php
@@ -1,20 +1,22 @@
-<head>
-    <link rel="stylesheet" href="css/foundation.css" />
-</head>
-<body>
-    <div class="row">
-        <div class="large-6 columns" style="padding:100px 0 50px 0;">
-            <form method="get" action="results.php">
-                <h1>DVD Search</h1>
-                <div class="row collapse">
-                    <div class="small-10 columns">
-                        <input type="text" placeholder="Search by title" name="title">
+<html>
+    <head>
+        <link rel="stylesheet" href="css/foundation.css" />
+    </head>
+    <body>
+        <div class="row">
+            <div class="large-6 columns" style="padding:100px 0 50px 0;">
+                <form method="get" action="results.php">
+                    <h1>DVD Search</h1>
+                    <div class="row collapse">
+                        <div class="small-10 columns">
+                            <input type="text" placeholder="Search by title" name="title">
+                        </div>
+                        <div class="small-2 columns">
+                            <input type="submit" class="button postfix" value="Search">
+                        </div>
                     </div>
-                    <div class="small-2 columns">
-                        <input type="submit" class="button postfix" value="Search">
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
+</html>
